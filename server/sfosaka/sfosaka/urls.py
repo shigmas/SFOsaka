e1db40a67fa4204a66429dbbea6ed474545f5109
Masa <wyan.git@futomen.net>
2016-07-12 13:38:42 -0700

    For gunicorn to work, added the staticfiles handler to the main urls.py

--- server/sfosaka/sfosaka/urls.py
+++ server/sfosaka/sfosaka/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'sfosaka.views.home', name='home'),
@@ -9,3 +11,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^mobapp/', include('mobapp.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
